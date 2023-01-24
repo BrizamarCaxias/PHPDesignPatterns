175f1555926d652548a0334f6219a6ac775cc912
Brizamar Caxias <brizamarcaxias@gmail.com>
2023-01-24 17:20:55 -0300

    Project inicitalization.

---
+++ Singleton/LogsSingleton.php
@@ -0,0 +1,16 @@
+<?php
+namespace Singleton;
+
+class LogsSingleton{
+
+    protected static $instancia;
+
+    public function gravarLog(array $dados){
+
+        $nomeArquivo = 'logs.txt';
+
+        $logsAnteriores =[];
+
+    }
+}
+
