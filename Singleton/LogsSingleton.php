10035c0b6673772577c5a22947d6024d851ba39f
Brizamar Caxias <brizamarcaxias@gmail.com>
2023-01-24 22:41:20 -0300

    Add: Função de gravação de Logs em arquivos TXT.

--- Singleton/LogsSingleton.php
+++ Singleton/LogsSingleton.php
@@ -11,6 +11,19 @@ class LogsSingleton{
 
         $logsAnteriores =[];
 
+        if(filesize($nomeArquivo) > 0){
+            $conteudoDoArquivo = file_get_contents($nomeArquivo);
+            $logsAnteriores = json_decode($conteudoDoArquivo);
+
+        }
+
+        $logsAnteriores[] = $dados;
+        $arquivo = fopen($nomeArquivo,'w');
+        fwrite($arquivo,json_encode($logsAnteriores));
+        fclose($arquivo);
+
     }
+
+    
 }
 
